e04389f685bbf485fa0e82868e1b6f61f4c77824
Qsior <lukaszbojes@gmail.com>
2017-11-04 23:35:03 +0100

    Added examples

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,35 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        int value = 3;
+//
+//        if(value == 1){
+//            System.out.println("Value was 1");
+//        } else if(value == 2){
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 3;
+
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("Was 3 or 4 or 5");
+                System.out.println("It was : " + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2 or 3 or 4 or 5");
+                break;
+        }
     }
 }
